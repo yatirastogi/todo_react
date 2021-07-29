4933b8c4334b24ad5e7df100e3f3f4258fe7c4c7
yatirastogi <63449149+yatirastogi@users.noreply.github.com>
2021-07-29 23:38:52 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Todo from './todo';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<Todo />, document.getElementById('root'));
+
+
